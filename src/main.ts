dc1cbcded814b56e618113bc551ae9ed86aef52b
akashin <kashin.andrej@gmail.com>
2017-11-15 23:48:18 +0000

    Draw a simple field.

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,82 @@
-var GRID_W = 8;
-var GRID_H = 8;
+const GRID_W = 8;
+const GRID_H = 8;
 
-var JEWEL_W = 10;
-var JEWEL_H = 10;
+const JEWEL_W = 40;
+const JEWEL_H = 40;
 
-// Jewel = {
-//     type: 1,
-// };
+class Jewel {
+    type: number;
+    shape: createjs.Shape;
 
-// function createField(W, H) {
-//     var Jewel[10][10];
-// }
+    constructor(type: number) {
+        this.type = type;
+        this.shape = getShapeForType(type);
+    }
+};
+
+type Row = Array<Jewel>;
+type Grid = Array<Row>;
+
+function getShapeForType(type: number) {
+    let shape = new createjs.Shape();
+    shape.graphics
+        .setStrokeStyle(1)
+        .beginStroke("#000000")
+
+    if (type === 0) {
+        shape.graphics.beginFill("red");
+        shape.graphics.drawCircle(0, 0, JEWEL_H / 2);
+        shape.regX = -JEWEL_H / 2;
+        shape.regY = -JEWEL_W / 2;
+    } else if (type === 1) {
+        shape.graphics.beginFill("yellow");
+        shape.graphics.drawRect(0, 0, JEWEL_H, JEWEL_W);
+    } else if (type === 2) {
+        shape.graphics.beginFill("green");
+        shape.graphics.drawPolyStar(0, 0, JEWEL_H / 2, 5, 0, -90);
+        shape.regX = -JEWEL_H / 2;
+        shape.regY = -JEWEL_W / 2;
+    }
+    return shape
+}
+
+function createGrid(W: number, H: number): Grid {
+    let field = new Array<Row>(H);
+    for (let i = 0; i < H; ++i) {
+        field[i] = Array<Jewel>(W);
+        for (let j = 0; j < W; ++j) {
+            field[i][j] = new Jewel(randomInt(0, 2));
+            field[i][j].shape.x = i * JEWEL_W;
+            field[i][j].shape.y = j * JEWEL_H;
+        }
+    }
+    return field;
+}
 
 function main() {
-    // document.body.innerHTML = "Hello, world!";
+    let stage = new createjs.StageGL('BejeweledStage');
+    stage.mouseEnabled = true;
 
-    var canvas; //Will be linked to the canvas in our index.html page
-    var stage; //Is the equivalent of stage in AS3; we'll add "children" to it
+    let grid_container = new createjs.Container();
 
-    // Graphics
-    //[Background]
+    let grid = createGrid(GRID_H, GRID_W);
+    for (let i = 0; i < grid.length; ++i) {
+        for (let j = 0; j < grid[i].length; ++j) {
+            grid_container.addChild(grid[i][j].shape);
+        }
+    }
 
-    var bg; //The background graphic
-
-    //[Title View]
-
-    var main_background; //The Main Background
-
-    stage = new createjs.StageGL('BejeweledStage');
-    stage.mouseEventsEnabled = true;
-
-
-    var g = new createjs.Graphics();
-    g.setStrokeStyle(1);
-    g.beginStroke("#000000");
-    g.beginFill("red");
-    g.drawCircle(100, 100, 30);
-
-    var shape = new createjs.Shape(g);
-    stage.addChild(shape);
-
-    shape.cache(0, 0, 600, 400);
-
+    grid_container.cache(0, 0, 640, 480);
+    stage.addChild(grid_container);
     stage.update();
-    // createjs.Ticker.addListener(stage);
 
-    createjs.Tween.get(shape, { loop: true })
-        .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
-        .to({ alpha: 0, y: 225 }, 100)
-        .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-        .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+    // createjs.Tween.get(shape, { loop: true })
+    //     .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+    //     .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
+    //     .to({ alpha: 0, y: 225 }, 100)
+    //     .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
+    //     .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
 
-    createjs.Ticker.setFPS(60);
+    createjs.Ticker.framerate = 60;
     createjs.Ticker.addEventListener("tick", stage);
 }
